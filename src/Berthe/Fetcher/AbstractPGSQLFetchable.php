5757b8f3f3b8347719ec4bbb9772454ed6686d0a
remievaneos <remi@evaneos.com>
2014-04-17 17:22:38 +0200

    added a system to add only the columns / joins nedded for the query

--- src/Berthe/Fetcher/AbstractPGSQLFetchable.php
+++ src/Berthe/Fetcher/AbstractPGSQLFetchable.php
@@ -40,7 +40,7 @@ abstract class AbstractPGSQLFetchable extends AbstractFetchable
 
     protected function getCountByFetcher(Fetcher $fetcher)
     {
-        $query = $this->getQuery();
+        $query = $this->getQuery($fetcher);
 
         list($filterInReq, $filterToParameter) = $this->queryBuilder->buildFilters($fetcher);
 
@@ -58,7 +58,7 @@ SQL;
 
     protected function getIdsByFetcher(Fetcher $fetcher)
     {
-        $query = $this->getQuery();
+        $query = $this->getQuery($fetcher);
 
         list($filterInReq, $filterToParameter) = $this->queryBuilder->buildFilters($fetcher);
         $sortInReq = $this->queryBuilder->buildSort($fetcher);
@@ -98,4 +98,66 @@ SQL;
 
         return $this->db->fetchCol($sql, $filterToParameter);
     }
+    
+    
+    protected function getSelectedColumnsjoins($column, $mainTableAlias, array $selectedColumns = array(), $selectedJoin = array(0, -1)) {
+        $columns = $this->getColumns();
+        if ($columns != null && array_key_exists($column, $columns)) {
+            $selectedColumns[$column] = $columns[$column]['select'];
+    
+            $join = $columns[$column]['join'];
+            if ($join[0]<$selectedJoin[0]) {
+                $selectedJoin[0] = $join[0];
+            }
+            if ($join[1]>$selectedJoin[1]) {
+                $selectedJoin[1] = $join[1];
+            }
+        } else {
+            $selectedColumns[$column] = $mainTableAlias.'.'.$column;
+        }
+    
+        return array($selectedColumns, $selectedJoin);
+    }
+    
+    protected function getQueryParameters(Fetcher $fetcher = null, $mainTableAlias='ref')
+    {
+        $select = '';
+        $from = '';
+    
+        if ($fetcher != null) {
+    
+            $selectedColumns = array();
+            $selectedJoin = array(0, -1);
+    
+            $filters = $fetcher->getFilters();
+            foreach ($filters as $filter) {
+                list($selectedColumns, $selectedJoin) = $this->getSelectedColumnsjoins($filter[3], $mainTableAlias, $selectedColumns, $selectedJoin);
+            }
+    
+            $sorts = $fetcher->getSorts();
+            foreach ($sorts as $column=>$sortOrder) {
+                list($selectedColumns, $selectedJoin) = $this->getSelectedColumnsjoins($column, $mainTableAlias, $selectedColumns, $selectedJoin);
+            }
+    
+            if (count($selectedColumns)>0) {
+                $select = ', '.implode(', ', $selectedColumns);
+            }
+            if ($selectedJoin[1]>0) {
+                $joins = $this->getJoins();
+                $selectedJoins = array_slice($joins, $selectedJoin[0], $selectedJoin[1]+1);
+                $from = implode(' ', $selectedJoins);
+            }
+        }
+    
+        return array($select, $from);
+    }
+    
+    protected function getColumns() {
+        return array();
+    }
+    
+    protected function getJoins() {
+        return array();
+    }
+    
 }
