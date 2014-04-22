cd89b6156fe0b5d145f70df4d8c1264de05c15d8
SelrahcD <c.desneuf@gmail.com>
2014-04-22 17:58:36 +0200

    Add IdentityComposer

---
+++ src/Berthe/Composition/IdentityComposer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Berthe\Composition;
+
+class IdentityComposer extends AbstractComposer {
+
+    public function compose(array $objects = array(), array $embededModels = array())
+    {
+        return $objects;
+    }
+}
