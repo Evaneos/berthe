e590229f91389fb0d8472b8d77f2b4790b225941
Johan <johan@evaneos.com>
2014-11-17 13:30:00 +0100

    Fix typo

--- src/Berthe/AbstractBuilder.php
+++ src/Berthe/AbstractBuilder.php
@@ -1,24 +1,24 @@
 <?php
 namespace Berthe;
 
-use DateTime;
-use DateTimeZone;
+use \DateTime;
+use \DateTimeZone;
 
 abstract class AbstractBuilder implements Builder {
 
-	/**
-	 * @var the timezone to which the date will be transformed
-	 */ 
-	private $timeZone = 'Europe/Paris';
+    /**
+     * @var the timezone to which the date will be transformed
+     */ 
+    private $timeZone = 'Europe/Paris';
 
-	/**
-	 * Set the timezone
-	 * 
-	 * @param the time zone to which the date will be transformed.
-	 */
-	public function setTimeZone($timeZone) {
-		$this->timeZone = $timeZone;
-	}
+    /**
+     * Set the timezone
+     * 
+     * @param the time zone to which the date will be transformed.
+     */
+    public function setTimeZone($timeZone) {
+        $this->timeZone = $timeZone;
+    }
 
     /**
      * Transform a date string into a DateTime object using GMT timezone
