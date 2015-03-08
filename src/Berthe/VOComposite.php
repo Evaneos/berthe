b0a7aecafa1c365968615dd7b92d32be95219562
Christophe Hurpeau <christophe@hurpeau.com>
2015-03-08 20:09:12 +0100

    composer embeds

--- src/Berthe/VOComposite.php
+++ src/Berthe/VOComposite.php
@@ -10,25 +10,39 @@ class VOComposite
      */
     private $component;
 
+    /**
+     * @param $component
+     */
     public function __construct($component)
     {
         $this->component = $component;
     }
 
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return self|mixed
+     */
     public function __call($name, $arguments)
     {
-        if(method_exists($this->component, $name)) {
-            $ret = call_user_func_array(array($this->component, $name), $arguments);
-
-            // Don't break setter chaining
-            if($ret === $this->component) {
-                return $this;
-            }
-
-           return $ret;
-        }
-        else {
+        if (!method_exists($this->component, $name)) {
             throw new \BadMethodCallException(sprintf('%s::%s doesn\'t exist', get_class($this->component), $name));
         }
+        $ret = call_user_func_array(array($this->component, $name), $arguments);
+
+        // Don't break setter chaining
+        if ($ret === $this->component) {
+            return $this;
+        }
+
+        return $ret;
+    }
+
+    /**
+     * @return object
+     */
+    protected function getComponent()
+    {
+        return $this->component;
     }
 }
