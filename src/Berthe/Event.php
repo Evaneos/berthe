5ea7e15b97a5dbffebb429296946becf56e4821c
SelrahcD <c.desneuf@gmail.com>
2015-09-30 18:03:05 +0200

    Allow event to be transformed from/to json

--- src/Berthe/Event.php
+++ src/Berthe/Event.php
@@ -4,16 +4,45 @@ namespace Berthe;
 
 use League\Event\Event as LeagueEvent;
 
-final class Event extends LeagueEvent
+final class Event extends LeagueEvent implements \JsonSerializable
 {
+    /**
+     * @var array
+     */
     private $params;
 
+    /**
+     * @param string $eventName
+     * @param array $params
+     */
     public function __construct($eventName, array $params = array())
     {
         parent::__construct($eventName);
         $this->params = $params;
     }
 
+    /**
+     * @param $jsonEvent
+     * @return Event
+     */
+    static public function fromJson($jsonEvent)
+    {
+        $arrayEvent = json_decode($jsonEvent, true);
+
+        $eventParams = isset($arrayEvent['params']) && is_array($arrayEvent['params']) ? $arrayEvent['params'] : array();
+
+        if(!isset($arrayEvent['name']))
+        {
+            throw new \InvalidArgumentException('Json representation of an event must contain a name');
+        }
+
+        return new self($arrayEvent['name'], $eventParams);
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
     public function get($key)
     {
         if(!array_key_exists($key, $this->params)) {
@@ -21,5 +50,16 @@ final class Event extends LeagueEvent
         }
 
         return $this->params[$key];
+    }
+
+    /**
+     * @return string
+     */
+    public function jsonSerialize()
+    {
+        return json_encode(array(
+            'name' => $this->name,
+            'params' => $this->params
+        ));
     }
 }
